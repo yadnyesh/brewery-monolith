5737d6ce4828e1119304607e09f0404254125493
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-04-05 11:52:44 +0530

    Base Project

---
+++ target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,28 @@
+package guru.sfg.brewery.web.mappers;
+
+import guru.sfg.brewery.domain.Beer;
+import guru.sfg.brewery.web.model.BeerDto;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-04-05T11:48:54+0530",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+
+    @Autowired
+    @Qualifier("delegate")
+    private BeerMapper delegate;
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto)  {
+        return delegate.beerDtoToBeer( beerDto );
+    }
+}
---
+++ target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.sfg.brewery.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-04-05T11:48:54+0530",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+}
